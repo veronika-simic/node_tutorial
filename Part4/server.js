c6f1744d88460b8ca897f99d9bedd4d3f4dbd569
veronika-simic <veronikasimic21@gmail.com>
2023-01-16 14:41:49 +0100

    routing

--- Part4/server.js
+++ Part4/server.js
@@ -11,7 +11,21 @@ const server = http.createServer((req, res) => {
 
   res.setHeader("Content-Type", "text/html");
   // send html file
-  fs.readFile("./views/index.html", (err, data) => {
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "./about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
