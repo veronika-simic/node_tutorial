f6088816ef83a17ab0b4be7c7336e0f28cc6ca60
veronika-simic <veronikasimic21@gmail.com>
2023-01-17 08:58:43 +0100

    next middleware

--- part8/server.js
+++ part8/server.js
@@ -3,15 +3,23 @@ const express = require("express");
 // express app
 const app = express();
 
+app.set("view engine", "ejs");
 // listen for requests
 app.listen(3000);
 
 // after we run this the express does not know what to do next, even if we click the about page link nothing happens
-app.use((req, res) => {
+app.use((req, res, next) => {
   console.log("New request is made");
   console.log("host", req.hostname);
   console.log("path", req.path);
   console.log("method", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in the next");
+
+  next();
 });
 
 app.get("/", (req, res) => {
