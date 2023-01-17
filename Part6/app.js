ecb5e6b855971d381e06f5d63b22efff797278cc
veronika-simic <veronikasimic21@gmail.com>
2023-01-17 07:52:03 +0100

    redirects with express

--- Part6/app.js
+++ Part6/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 // express app
 
 const app = express();
@@ -8,10 +7,21 @@ app.listen(3000);
 
 app.get("/", (req, res) => {
   res.send("<p>Home page</p>");
-  res.sendFile("./part4/views/index.html", { root: __dirname }); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
+  res.sendFile("Part4/views/index.html", { root: __dirname }); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
 });
 
 app.get("/about", (req, res) => {
   res.send("<p>About page</p>");
-  res.sendFile("./part4/views/about.html", { root: __dirname });
+  res.sendFile("Part4/views/about.html", { root: __dirname });
+});
+
+// redirects in express --> redirects and set status code
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page --> this function fires every time if the request gets to this point in code
+// here we need to edit status code to 404
+app.use((req, res) => {
+  res.status(404).sendFile("Part4/views/404.html", { root: __dirname });
 });
