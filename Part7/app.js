7bf4388caa3d9eacf45e2c9a61f0829054595077
veronika-simic <veronikasimic21@gmail.com>
2023-01-17 08:25:27 +0100

    finish part 7

--- Part7/app.js
+++ Part7/app.js
@@ -10,18 +10,23 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index", {title: 'Home'}); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
+  const blogs = [
+    { title: "Whats new in JS", snippet: "Lorem ipsum" },
+    { title: "Whats new in CSS", snippet: "Lorem ipsum" },
+    { title: "Whats new in HTML", snippet: "Lorem ipsum" },
+  ];
+  res.render("index", { title: "Home", blogs }); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {title: 'About'});
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create", {title: 'Blog'});
+  res.render("create", { title: "Blog" });
 });
 // 404 page --> this function fires every time if the request gets to this point in code
 // here we need to edit status code to 404
 app.use((req, res) => {
-  res.status(404).render("404", {title: '404'});
+  res.status(404).render("404", { title: "404" });
 });
