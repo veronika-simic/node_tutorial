34119c99838e26d94c615221885d82ebcf89dd80
veronika-simic <veronikasimic21@gmail.com>
2023-01-17 07:40:38 +0100

    aditional links

--- Part6/app.js
+++ Part6/app.js
@@ -8,4 +8,10 @@ app.listen(3000);
 
 app.get("/", (req, res) => {
   res.send("<p>Home page</p>");
+  res.sendFile("./part4/views/index.html", { root: __dirname }); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
+});
+
+app.get("/about", (req, res) => {
+  res.send("<p>About page</p>");
+  res.sendFile("./part4/views/about.html", { root: __dirname });
 });
