e8787e33ee5d85dadac19b300fdbc54b468071c5
veronika-simic <veronikasimic21@gmail.com>
2023-01-17 08:54:12 +0100

    browser hangs

---
+++ part8/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+// after we run this the express does not know what to do next, even if we click the about page link nothing happens
+app.use((req, res) => {
+  console.log("New request is made");
+  console.log("host", req.hostname);
+  console.log("path", req.path);
+  console.log("method", req.method);
+});
+
+app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
