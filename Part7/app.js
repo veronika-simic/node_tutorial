48785c9135135179686ae598d1faaef3e1394086
veronika-simic <veronikasimic21@gmail.com>
2023-01-17 08:14:48 +0100

    pass data into views

--- Part7/app.js
+++ Part7/app.js
@@ -10,20 +10,18 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index", { root: __dirname }); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
+  res.render("index", {title: 'Home'}); // we need to tell express where this path is relative from so we pass an object, what is this path relative to
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { root: __dirname });
+  res.render("about", {title: 'About'});
 });
 
-// redirects in express --> redirects and set status code
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", {title: 'Blog'});
 });
-
 // 404 page --> this function fires every time if the request gets to this point in code
 // here we need to edit status code to 404
 app.use((req, res) => {
-  res.status(404).render("404.", { root: __dirname });
+  res.status(404).render("404", {title: '404'});
 });
